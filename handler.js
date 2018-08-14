1a19f320b25c6dcb5a048f95028efede9a26f638
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-08-14 15:01:58 +0300

    4.2 - Crear cola y mandar mensajes

--- handler.js
+++ handler.js
@@ -1,17 +1,37 @@
 'use strict';
 
 const uuidv1 = require('uuid/v1');
+const AWS = require('aws-sdk');
+
+var sqs = new AWS.SQS({ region: process.env.REGION });
+const QUEUE_URL = process.env.PENDING_ORDER_QUEUE;
 
 module.exports.hacerPedido = (event, context, callback) => {
 	console.log('HacerPedido fue llamada');
 	const orderId = uuidv1();
 
-	const response = {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: `El pedido fue registrado con el numero de orden: ${orderId}`
-		})
+	const params = {
+		MessageBody: JSON.stringify({ orderId: orderId }),
+		QueueUrl: QUEUE_URL
 	};
 
-	callback(null, response);
+	sqs.sendMessage(params, function(err, data) {
+		if (err) {
+			sendResponse(500, err, callback);
+		} else {
+			const message = {
+				orderId: orderId,
+				messageId: data.MessageId
+			};
+			sendResponse(200, message, callback);
+		}
+	});
 };
+
+function sendResponse(statusCode, message, callback) {
+	const response = {
+		statusCode: statusCode,
+		body: JSON.stringify(message)
+	};
+	callback(null, response);
+}
