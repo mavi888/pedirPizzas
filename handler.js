48e8c07306f0c963660e816d0145faa4311a3a63
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-08-14 14:36:46 +0300

    3.3 - Empezando con el lab

---
+++ handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const uuidv1 = require('uuid/v1');
+
+module.exports.hacerPedido = (event, context, callback) => {
+	const orderId = uuidv1();
+
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: `El pedido fue registrado con el numero de orden: ${orderId}`
+		})
+	};
+
+	callback(null, response);
+};
