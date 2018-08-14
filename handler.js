4c631b74c46dc53b626609df3d6af2f06b6a5091
Marcia Villalba <marciavillalba@Marcias-MacBook-Pro.local>
2018-08-14 15:19:43 +0300

    4.5 - Lambda integrada con SQS

--- handler.js
+++ handler.js
@@ -28,6 +28,13 @@ module.exports.hacerPedido = (event, context, callback) => {
 	});
 };
 
+module.exports.prepararPedido = (event, context, callback) => {
+	console.log('Preparar pedido fue llamada');
+
+	console.log(event);
+	callback();
+};
+
 function sendResponse(statusCode, message, callback) {
 	const response = {
 		statusCode: statusCode,
